26d2ac9f0b24e7b49a4471e838f52d570135f845
Mark Stillwell <mark@stillwell.me>
2015-08-27 22:48:55 +0100

    small change to possibly enable orchestrator...

--- conpaas-worker.sh
+++ conpaas-worker.sh
@@ -19,6 +19,17 @@ if [ $(file -b --mime-type $user_data_file) = 'application/x-gzip' ]; then
 	gunzip $user_data_file.gz
 fi
 
+# set up environment for maxeler orchestrator
+if [ -d /dev/infiniband ]; then
+    echo 1 > /proc/sys/net/ipv4/ip_forward
+    ip link add ib0 type dummy
+    ip address add 192.168.0.0/24 broadcast 192.168.0.255 dev ib0
+    ip link set ib0 up
+    route add 192.168.0.10 gw 172.17.42.1
+    route add 192.168.0.1 gw 172.17.42.1
+    iptables -t nat -I POSTROUTING -o eth0 -j MASQUERADE
+fi
+
 if [ ! -s $user_data_file ]; then
 	echo "No user-data available"
 elif head -1 $user_data_file | egrep -v '^#!'; then
